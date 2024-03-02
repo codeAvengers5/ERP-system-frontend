588c6d407aaed39f364ea0d5b3f41910c170393c
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-03-02 16:25:15 +0300

    fix: import resolve

--- public/images/index.js
+++ public/images/index.js
@@ -3,8 +3,8 @@ import logo from "./logo.png";
 import login_banner from "./login-banner.png";
 import Employ1 from "./Employ1.png";
 import Employ2 from "./manager2.png";
-import manager1 from "./manager1.png";
+import manager1 from "./manger1.png";
 import manager2 from "./manager2.png";
-import manager3 from "./manager3.png";
+import manager3 from "./manger3.png";
 
 export { logo, login_banner, Employ1, Employ2, manager1, manager2, manager3 };
