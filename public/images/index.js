44cb92b7604c67b14b6a3828420c5be49ae847fc
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-03-02 16:04:53 +0300

    fix: prettier format

--- public/images/index.js
+++ public/images/index.js
@@ -4,4 +4,4 @@ import login_banner from "./login-banner.png";
 import Employ1 from "./Employ1.png";
 import Employ2 from "./manager2.png";
 
-export { logo, login_banner,Employ1, Employ2 };
+export { logo, login_banner, Employ1, Employ2 };
