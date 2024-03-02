7f8056b8d955292c04be53144dab8ce4dc92688b
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-03-02 15:27:32 +0300

    fix: resolve conflict

--- src/app/employee/layout.jsx
+++ src/app/employee/layout.jsx
@@ -1,4 +1,3 @@
-
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
@@ -10,6 +9,7 @@ export default function DashboardLayout({ children }) {
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <Navbar />
           <main>
+            <div className="hideScrollbar  fixed top-[100px] h-full  overflow-x-auto pb-20 md:top-[56px] lg:left-[240px]">
               {children}
             </div>
           </main>
