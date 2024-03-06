5d3e113abce0af4a023af410f5c8f56693a092b8
EdlawitG <eduedil30@gmail.com>
2024-03-06 16:09:37 +0300

    feat:login_functionality

--- src/slices/auth.js
+++ src/slices/auth.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-unresolved */
 "use client";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import localStorage from "redux-persist/es/storage";
