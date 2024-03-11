493115e53aa4645ab7fd7738574f290a836c9818
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-03-11 12:19:30 +0300

    fix:import updated

--- src/app/itadmin/page.jsx
+++ src/app/itadmin/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Button from "@/components/Button";
 import React from "react";
-import Image from "/next/image"
+import Image from "/next/image";
 import { filter, plus } from "../../../public/icons";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
